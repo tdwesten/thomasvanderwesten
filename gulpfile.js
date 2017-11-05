b16ed2b8842ba6d2726bfc5fe20ce72a8187c775
Thomas van der Westen <thomas@jannesmannes.nl>
2017-11-05 22:51:12 +0100

    Favicons added

--- gulpfile.js
+++ gulpfile.js
@@ -8,12 +8,35 @@ const browserSync     = require( 'browser-sync' ).create();
 const autoprefixer    = require( 'gulp-autoprefixer' );
 const imagemin        = require( 'gulp-imagemin' );
 const imageminMozjpeg = require( 'imagemin-mozjpeg' );
+const favicons        = require( 'gulp-favicons' ),
+      inject          = require( 'gulp-inject' ),
+      gulpUtil        = require( 'gulp-util' ),
+      del             = require( 'del' ),
+      decode          = require( 'decode-html' );
 
 const siteRoot = '_site';
 const files    = {
     scss: 'assets/scss/*.scss',
+    favicon: {
+        src: 'assets/images/favicon/favicon.png',
+        dest: 'assets/images/favicon',
+        html_src: 'favicon-temp.html',
+        file_dest: '_includes/favicon.html',
+        folder_dest: '_includes/',
+    },
     images: [ 'assets/images/*.jpg', '!assets/images/*-larger.jpg', '!assets/images/*-large.jpg', '!assets/images/*-small.jpg', '!assets/images/*-smaller.jpg' ],
     imagesall: [ 'assets/images/*.jpg' ]
+};
+
+const projectConfig = {
+    appName: 'Thomas van der Westen',
+    appDescription: '',
+    url: 'http://thomasvanderwesten.nl',
+    version: 1.0,
+    developerName: 'Thomas van der Westen',
+    developerURL: 'http://thomasvanderwesten.nl',
+    background: '#fff',
+    path: '/'
 };
 
 gulp.task( 'css', () => {
@@ -52,7 +75,7 @@ gulp.task( 'serve', () => {
 
 gulp.task( 'default', [ 'css', 'jekyll', 'serve' ] );
 
-gulp.task( 'imagemin', ['imagemin-allfiles'], function () {
+gulp.task( 'imagemin', [ 'imagemin-allfiles' ], function () {
     return gulp.src( files.images )
     .pipe( imagemin( [
         imageminMozjpeg( {
@@ -72,6 +95,51 @@ gulp.task( 'imagemin-allfiles', function () {
         } )
     ] ) )
     .pipe( gulp.dest( 'assets/images' ) );
+} );
+
+/**
+ * Generates favicon files used by various browsers and OS's.
+ * Requires the `clean` task to run first.
+ * For creating a Material Design icon, I recommend using
+ */
+gulp.task( 'favicon-generate', function () {
+    return gulp.src( files.favicon.src )
+    .pipe( favicons( {
+        appName: projectConfig.appName,
+        appDescription: projectConfig.appDescription,
+        url: projectConfig.url,
+        version: projectConfig.version,
+        developerName: projectConfig.developerName,
+        developerURL: projectConfig.developerURL,
+        background: projectConfig.background,
+        path: files.favicon.dest,
+        display: 'standalone',
+        orientation: 'portrait',
+        start_url: '/',
+        logging: false,
+        online: false,
+        html: files.favicon.html_src,
+        pipeHTML: true,
+        replace: true
+    } ) )
+    .on( 'error', gulpUtil.log )
+    .pipe( gulp.dest( files.favicon.dest ) );
+} );
+
+gulp.task( 'inject-favicon', [ 'favicon-generate' ], function () {
+    gulp.src( files.favicon.file_dest )
+    .pipe( inject( gulp.src( [ './images/favicon/' + files.favicon.html_src ] ), {
+        starttag: '<!-- inject:head:{{ext}} -->',
+        transform: function ( filePath, file ) {
+            var content = file.contents.toString( 'utf8' ); // return file contents as string
+            return decode( content );
+        }
+    } ) )
+    .pipe( gulp.dest( files.favicon.folder_dest ) );
+} );
+
+gulp.task( 'clean-favicon', [ 'inject-favicon' ], function () {
+    return del.sync( [ './assets/images/favicon/favicon-temp.html' ] );
 } );
 
 const Logger = ( buffer ) => {
